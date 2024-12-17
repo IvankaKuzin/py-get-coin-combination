81243855a1b61aca3f899a5ef8091f024f8dd131
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-17 19:59:10 +0200

    Changed style-code

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,7 @@ import pytest
 
 from app.main import get_coin_combination
 
+
 @pytest.mark.parametrize(
     "cents,resul",
     [
@@ -9,8 +10,8 @@ from app.main import get_coin_combination
         (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2]),
-        (100, [0, 0, 0, 4])
-    ]
+        (100, [0, 0, 0, 4]),
+    ],
 )
 def test_get_coin_combination(cents: int, resul: list) -> None:
     assert get_coin_combination(cents) == resul
